3ce085bc06b8e8e1db2154b6ebf39fd062822f1f
Edouard Rav <edravelo@gmail.com>
2026-04-08 20:12:53 +0200

    frontend: add nuxt-ui

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <UHeader title="Ranuboy - Rails + Nuxt boilerplate 🔥"></UHeader>
+    <UMain class="main">
+			<p>Let's build something!</p>
+    </UMain>
+  </UApp>
 </template>
+
+<style scoped>
+.main {
+  padding: 30px;
+}
+</style>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxt/ui"],
+	css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
   devServer: {
     port: 3001
